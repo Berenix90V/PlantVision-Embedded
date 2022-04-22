cfb38330edcd1b460d0cba4fe5da02b406b94449
Veronica Zanella <zanella.veronica@outlook.it>
2022-04-22 09:42:51 +0300

    humidity and temperature sensor added

--- esp32/fs/init.js
+++ esp32/fs/init.js
@@ -6,11 +6,14 @@ load('api_mqtt.js');
 load('api_adc.js');
 load('api_timer.js');
 load('api_sys.js');
+load('api_dht.js');
 
 let light_pin = 34;
+let temperature_humidity_pin = 13;
+let dht = DHT.create(temperature_humidity_pin, DHT.DHT11);
 let led = Cfg.get('board.led1.pin');              // Built-in LED GPIO number
 let onhi = Cfg.get('board.led1.active_high');     // LED on when high?
-let state = {on: false, uptime: 0, light:0.0};  // Device state
+let state = {on: false, uptime: 0, light:0.0, temperature:0.0, humidity:0.0};  // Device state
 
 let setLED = function(on) {
   let level = onhi ? on : !on;
@@ -18,15 +21,20 @@ let setLED = function(on) {
   print('LED on ->', on);
 };
 
+// Set ouptut pins
 GPIO.set_mode(led, GPIO.MODE_OUTPUT);
 setLED(state.on);
 
+// Set input pins
 GPIO.set_mode(light_pin, GPIO.MODE_INPUT);
+GPIO.set_mode(temperature_humidity_pin, GPIO.MODE_INPUT);
 
 Timer.set(5*1000, Timer.REPEAT, function(){
   state.uptime = Sys.uptime();
   state.ram_free = Sys.free_ram();
-  state.light = ADC.read(light_pin)/4096;
+  state.light = ADC.read(light_pin)/4096; // 2667-4095
+  state.temperature = dht.getTemp();
+  state.humidity = dht.getHumidity();
   print(JSON.stringify(state));
 }, null);
 // Update state every second, and report to cloud if online
